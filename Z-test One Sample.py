9107f9cc8f5535cc7d6c54b4413a7e25b40a4969
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-17 16:40:36 +0530

    Update Z-test One Sample.py

--- Z-test One Sample.py
+++ Z-test One Sample.py
@@ -1,3 +1,5 @@
+# Calculating Z value:
+
 x =  
 xbar = np.mean(x)
 mu = 
@@ -6,3 +8,13 @@ n = len(x)
 
 z_cal = (xbar - mu) / (sigma / np.sqrt(n))
 print("Z calculated value is: ", round(z_cal,2))
+
+
+
+# Calculating p value:
+
+# FOR ONE TAILED TEST
+print(round(stats.norm.sf(abs(z_cal)),2))
+
+# FOR TWO TAILED TEST
+print(round(stats.norm.sf(abs(z_cal))*2,2))
