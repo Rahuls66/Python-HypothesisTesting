580f0122639f5c83aef6be978c716f57feb19672
Rahul Shah <rahul.shah4513@gmail.com>
2021-11-19 17:20:32 +0530

    Update Z-test P-Value Calculation.py

--- Z-test P-Value Calculation.py
+++ Z-test P-Value Calculation.py
@@ -1,10 +1,10 @@
-# Calculating Z value:
+# -- CALCULATING Z VALUE --
 import numpy as np
 import scipy.stats as stats
 
-x =  
+x =  input()
 xbar = np.mean(x)
-mu = 
+mu = input()
 sigma = np.std(x, ddof = 1)
 n = len(x)
 
@@ -12,10 +12,10 @@ z_cal = (xbar - mu) / (sigma / np.sqrt(n))
 print("Z calculated value is: ", round(z_cal,2))
 
 
+# -- CALCULATING P-VALUE FOR z_cal --
 
-# Calculating p value for z_cal:
-# FOR ONE TAILED TEST
+# -- FOR ONE TAILED TEST
 print(round(stats.norm.sf(abs(z_cal)),2))
 
-# FOR TWO TAILED TEST
+# -- FOR TWO TAILED TEST
 print(round(stats.norm.sf(abs(z_cal))*2,2))
