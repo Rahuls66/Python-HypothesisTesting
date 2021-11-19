ac5c113f533e37bd9dbc460aea46c43468d6a8cc
Rahul Shah <rahul.shah4513@gmail.com>
2021-11-19 17:19:28 +0530

    Update Z-test Confidence Interval.py

--- Z-test Confidence Interval.py
+++ Z-test Confidence Interval.py
@@ -1,15 +1,21 @@
-# Z-test Confidence Interval
-
-import scipy.stats as stats
+# -- CALCULATING Z VALUE -- 
 import numpy as np
+import scipy.stats as stats
 
-alpha = 
+x = input()
+xbar = np.mean(x)
+mu = input()
+sigma = np.std(x, ddof = 1)
+n = len(x)
+
+z_cal = (xbar - mu) / (sigma / np.sqrt(n))
+print("Z calculated value is: ", round(z_cal,2))
+
+alpha = input()
 z_alphaby2 = stats.norm.isf(alpha/2)
 
-x =  
-sigma = np.std(x, ddof = 1)
-xbar = np.mean(x)
 
+# -- CALCULATING CONFIDENCE INTERVAL -- 
 margin_error = z_alphaby2 * sigma
 
 lci = xbar - margin_error
