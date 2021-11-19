167cdc210be385435638c013144990e99be4fdb6
Rahul Shah <rahul.shah4513@gmail.com>
2021-11-19 17:15:23 +0530

    Create P Value Calculation.py

---
+++ P Value Calculation.py
@@ -0,0 +1,21 @@
+# Calculating Z value:
+import numpy as np
+import scipy.stats as stats
+
+x =  
+xbar = np.mean(x)
+mu = 
+sigma = np.std(x, ddof = 1)
+n = len(x)
+
+z_cal = (xbar - mu) / (sigma / np.sqrt(n))
+print("Z calculated value is: ", round(z_cal,2))
+
+
+
+# Calculating p value for z_cal:
+# FOR ONE TAILED TEST
+print(round(stats.norm.sf(abs(z_cal)),2))
+
+# FOR TWO TAILED TEST
+print(round(stats.norm.sf(abs(z_cal))*2,2))
